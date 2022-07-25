90a46cdefaaed422cb4328cad3694bafdf142ebc
GouthamNR <96580645+GouthamNR@users.noreply.github.com>
2022-07-25 16:13:22 +0530

    Update GitBasTest.java

--- src/main/java/gitbashProject/GitBasTest.java
+++ src/main/java/gitbashProject/GitBasTest.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("gitbash project");
 	System.out.println("gitbash commited");
 	System.out.println("creating new branch");
+	System.out.println("from github");
 }
 	
 }
